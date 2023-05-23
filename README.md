bf5044b9290efb3e280d3e716b7009e6513918d6
JoselineByJFS <88074256+JoselineByJFS@users.noreply.github.com>
2023-05-22 19:43:21 -0600

    invitacion

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# invitacionH
+# C42_Actividad de la maestra_Carreras de autos
+Actividad de la maestra
---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+function preload() {
+  
+}
+
+function setup() {
+  canvas = createCanvas(600,1200);
+  
+}
+
+function draw() {
+  background("pink");
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
